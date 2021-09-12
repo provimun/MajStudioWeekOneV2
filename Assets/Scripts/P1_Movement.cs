6f095f88497331e7dd588564609bf07e276f954a
Munro <mp5340@nyu.edu>
2021-09-12 18:06:30 -0400

    Game Objects and some Sprites

--- Assets/Scripts/P1_Movement.cs
+++ Assets/Scripts/P1_Movement.cs
@@ -7,12 +7,17 @@ public class P1_Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        public float movementspeed = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W)){
+            transform.position = new Vector3(transform.position.x, transform.position.y - movementspeed, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.S)){
+            transform.position = new Vector3(transform.position.x, transform.position.y + movementspeed, transform.position.z);
+        }
     }
 }
--- Assets/Scripts/P2_Movement.cs
+++ Assets/Scripts/P2_Movement.cs
@@ -7,12 +7,19 @@ public class P2_Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        public float movementspeed = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - movementspeed, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + movementspeed, transform.position.z);
+        }
     }
 }
