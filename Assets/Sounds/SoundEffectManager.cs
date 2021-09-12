588d6d0f2efa02af70a593de0a162eabdb913603
jaden <cep450@nyu.edu>
2021-09-12 19:11:03 -0400

    worked more on sound script

--- Assets/Sounds/SoundEffectManager.cs
+++ Assets/Sounds/SoundEffectManager.cs
@@ -4,27 +4,51 @@ using UnityEngine;
 
 public class SoundEffectManager : MonoBehaviour
 {
-
     //remember to init stuff in the editor!!! 
     //also, set sounds to decompress on load 
 
     AudioSource myAudioSource;
 
+    public AudioClip soundEffect;
+    public float audioScale;
+
+    bool hasPlayedSound = false; //safeguard
+    float prevX;
 
     // Start is called before the first frame update
     void Start()
     {
         myAudioSource = GetComponent<AudioSource>();
+        prevX = transform.position.x;
+    }
+
+    void Update() {
+
+        //if the object crosses x=0, ie if the signs are different, ie if they multiply to a negative number...
+            //now this is a nice trick. have to remember this one 
+        if((prevX * transform.position.x) < 0 && !hasPlayedSound) {
+            //..it's crossed the center, so play its sound! 
+            myAudioSource.PlayOneShot(soundEffect, audioScale); 
+            hasPlayedSound = true; 
+        }
+
+        //finally, update the previous x pos 
+        prevX = transform.position.x;
+
     }
 
     void OnTriggerEnter(Collider otherCollider) {
 
-        //if the collider is the one in the middle, play my sound 
-        
-        //myAudioSource.PlayOneShot(); 
+        //if the collider is the player, play my sound but fucked up 
+        //myAudioSource.PlayOneShot(soundEffect, audioScale); 
+        //hasPlayedSound = true; 
 
-        //if the collider is the player, play my sound but fucked up
+    }
 
+    //for resetting values every time this obstacle gets looped back to where it started 
+    public void resetMe() {
+        prevX = transform.position.x; 
+        hasPlayedSound = false; 
     }
 
 
